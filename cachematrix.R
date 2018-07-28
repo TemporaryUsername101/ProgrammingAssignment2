ea09e028dde0d496710f788cf51755ba90b61007
Unprocessable <rgbk73@gmail.com>
2018-07-29 01:00:07 +0200

    Added some more whitespace.

--- cachematrix.R
+++ cachematrix.R
@@ -8,32 +8,37 @@
 ## and caches the result so it doesn't require calculation again.
 ##
 
+## Creates a list of functions for the matrix 'x' that allows caching of its inverse.
+## Note: it will reset the inverse (i) if the matrix is set again.
+## - Input - The same as any accepted input for the R matrix() function
+## - Output - A list of functions that allow saving the state of the cached solve result
+##
+## Functions defined:
+## set - changes the matrix input of makeCacheMatrix
+## get - returns the matrix input of makeCacheMatrix
+## setinverse - stores the inverse of the matrix
+## getinverse - returns the stored inverse of the matrix if available
 makeCacheMatrix <- function(x = matrix()) {
-  ## Creates a list of functions for the matrix 'x' that allows caching of its inverse.
-  ## Note: it will reset the inverse (i) if the matrix is set again.
-  ## - Input - The same as any accepted input for the R matrix() function
-  ## - Output - A list of functions that allow saving the state of the cached solve result
-  
-  ## Functions defined:
-  ## set - changes the matrix input of makeCacheMatrix
-  ## get - returns the matrix input of makeCacheMatrix
-  ## setinverse - stores the inverse of the matrix
-  ## getinverse - returns the stored inverse of the matrix if available
-  
+  ## initialize i as NULL
   i <- NULL
+  
   set <- function(y) {
     ## this sets the x and i outside of this function
     x <<- y
     i <<- NULL
   }
+  
   ## this returns x stored in the makeCacheMatrix
   get <- function() x
+  
   ## this sets i to inverse outside the setinverse function
   ## https://www.rdocumentation.org/packages/base/versions/3.5.1/topics/assignOps
   ## the inverse variable is only available inside the setinverse environment
   setinverse <- function(inverse) i <<- inverse
+  
   ## this again looks up i in the makeCacheMatrix environment
   getinverse <- function() i
+  
   ## any functions added to the list here will be local to that list only ('=')
   ## in case you were wondering, that was meant to be an equal sign.
   list(set = set, get = get,
@@ -41,10 +46,12 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
+## This function returns the inverse matrix of a makeCacheMatrix 'x'.
+## Note that it will cache the result after calculating it the first time.
+## The next time the stored inverse is returned and a message displayed.
 cacheSolve <- function(x, ...) {
-  ## This function returns the inverse matrix of a makeCacheMatrix 'x'.
-  ## Note that it will cache the result after calculating it the first time.
-  ## The next time the stored inverse is returned and a message displayed.
+  
+  ## try to get the inverse
   i <- x$getinverse()
   
   ## if the calculation has not been done before i == NULL
@@ -53,6 +60,7 @@ cacheSolve <- function(x, ...) {
     message("returning cached inverse")
     return(i)
   }
+  
   ## if no inverse calculation was done previously, do it now
   matrix <- x$get()
   i <- solve(matrix, ...)
